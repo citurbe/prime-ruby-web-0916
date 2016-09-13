c1705e6223633074adbb8903b5eaf37c81d5aa54
Craig Iturbe <citurbe@gmail.com>
2016-09-13 00:13:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+
+return false if int <= 1
+
+is_prime = true
+i = 2
+while i < int
+  if int % i == 0
+    is_prime = false
+  end
+  i += 1
+end
+return is_prime
+end
